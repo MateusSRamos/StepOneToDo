c340f01f34983d7b1fbed1126d34353f5f295f57
Larissa Santos <larissa.santos@rdstation.com>
2025-08-21 20:44:35 -0300

    feat: add createItem and updateTask function

--- script.js
+++ script.js
@@ -8,12 +8,11 @@ function addTask(event) {
   event.preventDefault(); //Mantem o dado no input em caso de falha
 
   const list = JSON.parse(localStorage.getItem("list")) || [];
-  const itemIndex = list.length 
-  console.log(list);
-  console.log(itemIndex)
+  const itemIndex = list.length;
 
   const taskItem = document.createElement("label"); //Cria um novo elemento HTML chamado <label>
   taskItem.classList.add("task-item"); //define a classe como task-item
+  taskItem.id = `task-item-${itemIndex}`; //define o id do elemento como task-1, task-2, etc.
 
   const checkboxInput = document.createElement("input"); //Crie um novo elemento de entrada chamado <input>
   checkboxInput.type = "checkbox"; //configurando esse <input> para ser um checkbox.
@@ -33,7 +32,7 @@ function addTask(event) {
   const trashItem = document.createElement("i"); //cria um elemento <i>
   trashItem.classList.add("fa"); //Diz que ele usa a biblioteca Font Awesome
   trashItem.classList.add("fa-trash"); //especifica qual ícone vai aparecer
-  trashItem.addEventListener("click", () => deleteTask(itemIndex))
+  trashItem.addEventListener("click", () => deleteTask(itemIndex)); //clicar no ícone da lixeira, executa a função deleteTask
 
   taskItem.appendChild(checkboxInput); // coloca o checkboc dentro do taskItem
   taskItem.appendChild(fakeCheckboxInput); //Adiciona o fakeCheckboxInput, <span> checkbox estilizado.
@@ -43,32 +42,30 @@ function addTask(event) {
   buttonTrash.appendChild(trashItem); //coloca o ícone da lixeira dentro do botão
   taskBox.appendChild(taskItem); //adiciona todo o item da tarefa na caixa principal (taskBox)
 
-  // trashItem.addEventListener("click", deleteTask); //clicar no ícone da lixeira, executa a função deleteTask
-  checkboxInput.addEventListener("click", completeTask); // clicar no checkbox, execute a função completeTask
+  const item = {
+    itemIndex, //pega o número do item da lista
+    description: taskItemText.innerText, //pega o texto do parágrafo que foi digitado pelo usuário
+    status: false, //inicialmente a tarefa não está completa
+  };
+
+  createItem(list, item);
+
+  checkboxInput.addEventListener("click", (e) => updateTask(e, itemIndex)); // clicar no checkbox, execute a função updateTask
 
   newTaskInput.value = ""; // reseta o campo de texto
 }
 
 function deleteTask(itemIndex) {
-  console.log(itemIndex)
+  console.log(itemIndex); // TODO - Implementar a lógica de exclusão de tarefa
 }
 
-function completeTask(e) {
-  e.target.parentElement.classList.toggle("complete");
+function updateTask(e, itemIndex) {
+  console.log(e.target.checked, itemIndex); // TODO - Implementar a lógica de atualização de tarefa
 }
 
-
-
-// Salvar e Apresentar dados salvos em localStorage
-
-function saveItem() {
-  const tasks = [];
-
-  localStorage.setItem(
-    "itemNumber",
-    taskItem.value,
-    taskItemText.value,
-    taskBox.value
-  );
+function createItem(list, item) {
+  list.push(item); //adiciona o item à lista
+  localStorage.setItem("list", JSON.stringify(list));
 }
+
 function showItens() {}
