786f89a972f7e69c7b2aa6959dca7176c5ada60a
MateusSRamos <mateusramos.dev@gmail.com>
2025-08-22 00:20:22 -0300

    Implementation function ShowItens(localStorage.getItem)

--- script.js
+++ script.js
@@ -4,11 +4,15 @@ const newTaskInput = document.querySelector(".new-task input"); //digitar o nome
 
 buttonNewTask.addEventListener("click", addTask); //clica para criar uma nova tarefa.
 
+window.addEventListener("load", (event) => {
+  showItens();
+});
+
 function addTask(event) {
   event.preventDefault(); //Mantem o dado no input em caso de falha
 
-  const list = JSON.parse(localStorage.getItem("list")) || [];
-  const itemIndex = list.length;
+  const list = JSON.parse(localStorage.getItem("list")) || []; //pega os dados salvos com a chave/transforma o texto salvo em um array/objeto JavaScript./se não existir nada salvo ainda, ele cria uma lista vazia [] para não dar erro.
+  const itemIndex = list.length; //retorna o tamanho atual da lista.
 
   const taskItem = document.createElement("label"); //Cria um novo elemento HTML chamado <label>
   taskItem.classList.add("task-item"); //define a classe como task-item
@@ -55,7 +59,9 @@ function addTask(event) {
   newTaskInput.value = ""; // reseta o campo de texto
 }
 
-function deleteTask(itemIndex) {
+function deleteTask(itemIndex,e) {
+
+  e.target.parentElement.parentElement.remove
   console.log(itemIndex); // TODO - Implementar a lógica de exclusão de tarefa
 }
 
@@ -68,4 +74,43 @@ function createItem(list, item) {
   localStorage.setItem("list", JSON.stringify(list));
 }
 
-function showItens() {}
+function showItens() {
+  const list = JSON.parse(localStorage.getItem("list")) || [];
+  for (i = 0; i < list.length; i++) {
+    const taskItem = document.createElement("label"); //Cria um novo elemento HTML chamado <label>
+    taskItem.classList.add("task-item"); //define a classe como task-item
+
+    const checkboxInput = document.createElement("input"); //Crie um novo elemento de entrada chamado <input>
+    checkboxInput.type = "checkbox"; //configurando esse <input> para ser um checkbox.
+
+    const fakeCheckboxInput = document.createElement("span"); //cria um novo elemento <span>
+    fakeCheckboxInput.classList.add("fake-checkbox"); //cria uma classe chamada fake-checkbox nesse <span>.
+
+    const checkIcon = document.createElement("i"); //Cria um elemento HTML
+    checkIcon.classList.add("fa"); //Diz que ele usa a biblioteca Font Awesome
+    checkIcon.classList.add("fa-check"); //especifica qual ícone vai aparecer
+
+    const taskItemText = document.createElement("p"); //Crie um novo elemento <p>.
+    taskItemText.innerText = list[i].description; //Pega o que a pessoa digitou no campo de texto (newTaskInput.value) e escreva dentro desse parágrafo.
+
+    const buttonTrash = document.createElement("button"); //Cria um novo botão
+
+    const trashItem = document.createElement("i"); //cria um elemento <i>
+    trashItem.classList.add("fa"); //Diz que ele usa a biblioteca Font Awesome
+    trashItem.classList.add("fa-trash"); //especifica qual ícone vai aparecer
+    const itemIndex = list[i].itemIndex
+    trashItem.addEventListener("click", () => deleteTask(itemIndex)); //clicar no ícone da lixeira, executa a função deleteTask
+    
+
+    taskItem.appendChild(checkboxInput); // coloca o checkbox dentro do taskItem
+    taskItem.appendChild(fakeCheckboxInput); //Adiciona o fakeCheckboxInput, <span> checkbox estilizado.
+    fakeCheckboxInput.appendChild(checkIcon); //Coloca o ícone de check dentro do checkbox falso
+    taskItem.append(taskItemText); //adiciona o parágrafo com o texto do usuário dentro do item da tarefa.
+    taskItem.appendChild(buttonTrash); //Adiciona o botão de apagar
+    buttonTrash.appendChild(trashItem); //coloca o ícone da lixeira dentro do botão
+    taskBox.appendChild(taskItem); //adiciona todo o item da tarefa na caixa principal (taskBox)
+  }
+}
+
+//  localStorage.removeItem("list"); - remove a lista completa"
+//getItem() - pega uma chave e retorna o valor correspondente
